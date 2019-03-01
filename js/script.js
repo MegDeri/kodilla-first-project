441250d7d12acdf781dc5efcb3aba7a48d03905b
Magda Aderibigbe <magdaaderibigbe@gmail.com>
2019-03-01 17:41:38 +0200

    Changes style and html

--- js/script.js
+++ js/script.js
@@ -9,3 +9,12 @@ function closeSlideMenu(){
     document.getElementById('side-menu').style.width = "0";
     document.getElementById('main').style.marginLeft ="0";
 }
+/*var toggle = document.getElementById('top-menu');
+
+function toggleMenu() {
+    document.getElementById('side-menu').classList.toggle('show')
+  }
+
+  toggle.addEventListener("click", function() {
+    toggleMenu();
+  });*/
