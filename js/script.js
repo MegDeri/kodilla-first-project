7e42d1a7d148648fdd4cd276b0c07603f4961464
Magda Aderibigbe <magdaaderibigbe@gmail.com>
2019-03-18 13:25:18 +0200

    Amend js for modals

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 'use strict'
 
-
+//open & close side-bar
 function openSlideMenu(){
     document.getElementById('side-menu').style.width = "250px";
     document.getElementById('main').style.marginLeft ="250px";
@@ -23,3 +23,36 @@ function toggleMenu() {
   toggle.addEventListener("click", function() {
     toggleMenu();
   });
+
+  //close modal
+  function closeModal() {
+    document.getElementById('overlay').classList.remove('show')
+  }
+
+  document.querySelectorAll('#overlay .js--close-modal').forEach(function(btn) {
+    btn.addEventListener('click', function(e) {
+      e.preventDefault()
+      closeModal()
+    })
+  })
+
+  document.querySelector('#overlay').addEventListener('click', function(e) {
+    if(e.target === this) {
+      closeModal()
+    }
+  })
+
+  document.addEventListener('keyup', function(e) {
+    if(e.keyCode === 27) {
+      closeModal()
+    }
+  })
+
+  //open modal
+  function openModal(modal) {
+    document.querySelectorAll('#overlay > *').forEach(function(modal) {
+      modal.classList.remove('show')
+    })
+    document.querySelector('#overlay').classList.add('show')
+    document.querySelector(modal).classList.add('show')
+  }
