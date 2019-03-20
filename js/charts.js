d12157dbc637761bd91ab794f0ba0b6f3fd036c0
Magda Aderibigbe <magdaaderibigbe@gmail.com>
2019-03-20 15:32:30 +0200

    Final changes and adjust responsivity

--- js/charts.js
+++ js/charts.js
@@ -6,7 +6,7 @@ var chart = new Chart(ctx, {
     type: 'bar',
     data: {
         // 2
-        labels: ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10"],
+        labels: ["01", "02", "03", "04", "05", "06", "07", "08", "09" ],
         // 3
         datasets: [{
             // 4
@@ -15,19 +15,19 @@ var chart = new Chart(ctx, {
             backgroundColor: '#8DBEC8',
             borderColor: '#8DBEC8',
             // 6
-            data: [ 52, 51, 41, 94, 26, 6, 72, 9, 21, 88 ],
+            data: [ 340, 200, 260, 350, 415, 380, 280, 270, 300 ],
         },
         {
             label: "FTD",
             backgroundColor: '#F29E4E',
             borderColor: '#F29E4E',
-            data: [ 6, 72, 1, 0, 47, 11, 50, 44, 63, 76 ],
+            data: [ 400, 180, 301, 270, 455, 125, 200, 500, 310 ],
         },
         {
             label: "",
             backgroundColor: '#ffffff',
             borderColor: '#ffffff',
-            data: [ 59, 49, 68, 90, 67, 41, 13, 38, 48, 48 ],
+            data: [ 59, 49, 68, 90, 67, 41, 13, 38, 48, 600 ],
             // 7
             hidden: true,
         }]
