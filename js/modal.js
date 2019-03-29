96411430ddcc46a12a06a3bfd6a16536379ae5d1
Magda Aderibigbe <magdaaderibigbe@gmail.com>
2019-03-29 23:20:21 +0200

    Change style in sass

--- js/modal.js
+++ js/modal.js
@@ -1,37 +1,36 @@
 //close modal
 function closeModal() {
-    document.getElementById('overlay').classList.remove('show')
+  document.getElementById('overlay').classList.remove('show');
+}
+
+document.querySelectorAll('#overlay .js--close-modal').forEach(function(btn) {
+  btn.addEventListener('click', function(e) {
+    e.preventDefault();
+    closeModal();
+  });
+})
+
+document.querySelector('#overlay').addEventListener('click', function(e) {
+  if (e.target === this) {
+    closeModal();
   }
+})
 
-  document.querySelectorAll('#overlay .js--close-modal').forEach(function(btn) {
-    btn.addEventListener('click', function(e) {
-      e.preventDefault()
-      closeModal()
-    })
-  })
-
-  document.querySelector('#overlay').addEventListener('click', function(e) {
-    if(e.target === this) {
-      closeModal()
-    }
-  })
-
-  document.addEventListener('keyup', function(e) {
-    if(e.keyCode === 27) {
-      closeModal()
-    }
-  })
+document.addEventListener('keyup', function(e) {
+  if (e.keyCode === 27) {
+    closeModal();
+  }
+})
 
   //open modal
-  function openModal(modal) {
-    document.querySelectorAll('#overlay > *').forEach(function(item) {
-      item.classList.remove('show')
-    })
-    document.querySelector('#overlay').classList.add('show')
-    document.querySelector(modal).classList.add('show')
-  }
+function openModal(modal) {
+  document.querySelectorAll('#overlay > *').forEach(function(item) {
+    item.classList.remove('show');
+  })
+  document.querySelector('#overlay').classList.add('show');
+  document.querySelector(modal).classList.add('show');
+}
 
-
-  document.getElementById('yourManager').addEventListener('click', function(){
-    openModal('#myModal');
-  });
+document.getElementById('yourManager').addEventListener('click', function() {
+  openModal('#myModal');
+});
--- js/script.js
+++ js/script.js
@@ -1,27 +1,23 @@
 'use strict'
+var toggle = document.querySelector("#top-menu a");
 
 //open & close side-bar
-function openSlideMenu(){
-    document.getElementById('side-menu').style.width = "250px";
-    document.getElementById('main').style.marginLeft ="250px";
+function openSlideMenu() {
+  document.getElementById('side-menu').style.width = "250px";
+  document.getElementById('main').style.marginLeft ="250px";
 }
 
-function closeSlideMenu(){
-    document.getElementById('side-menu').style.width = "0";
-    document.getElementById('main').style.marginLeft ="0";
+function closeSlideMenu() {
+  document.getElementById('side-menu').style.width = "0";
+  document.getElementById('main').style.marginLeft ="0";
 }
 
 document.querySelector("#side-menu .b-close").addEventListener("click", closeSlideMenu);
 document.querySelector("#top-menu a").addEventListener("click", openSlideMenu);
 
-var toggle = document.querySelector("#top-menu a");
-
 function toggleMenu() {
-    document.getElementById('side-menu').classList.toggle('showon')
-  }
-
-  toggle.addEventListener("click", function() {
-    toggleMenu();
-  });
-
-  
+  document.getElementById('side-menu').classList.toggle('showon')
+}
+toggle.addEventListener("click", function() {
+  toggleMenu();
+});
